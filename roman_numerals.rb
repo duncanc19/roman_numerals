070b9829b3c922f960feaec4ee3d16a720a3ec8a
duncanc19 <duncancarter93@gmail.com>
2020-09-25 09:53:28 +0100

    Add decimal lookup, remove conditional block  - refactoring

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,7 +1,7 @@
 class Roman_Numerals
   def convert(num)
     return "" if !num
-    number_lookup = {
+    single_digit_lookup = {
         0 => "",
         1 => "I",
         2 => "II",
@@ -13,20 +13,23 @@ class Roman_Numerals
         8 => "VIII",
         9 => "IX"
     }
-    if num >= 80
-      "LXXX" + number_lookup[num % 10]
-    elsif num >= 70
-      "LXX" + number_lookup[num % 10]
-    elsif num >= 60
-      "LX" + number_lookup[num % 10]
-    elsif num >= 50
-        "L" + number_lookup[num % 10]
-    elsif num >= 40
-        "XL" + number_lookup[num % 10]
-    elsif num >= 10
-       "X" * (num / 10) + number_lookup[num % 10]
+
+    decimal_lookup = {
+      10 => "X",
+      20 => "XX",
+      30 => "XXX",
+      40 => "XL",
+      50 => "L",
+      60 => "LX",
+      70 => "LXX",
+      80 => "LXXX",
+      90 => "XC"
+    }
+    
+    if num >= 10
+      decimal_lookup[num.floor(-1)] + single_digit_lookup[num % 10]
     else
-        number_lookup[num]
+      single_digit_lookup[num]
     end
   end 
 
